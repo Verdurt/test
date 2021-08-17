276f85300e48b4aaf71c9caf5d729e21554f0c74
fangshuangshuang <fangshuangshuang@meituan.com>
2021-08-17 15:30:24 +0800

    hard commit测试

--- src/main/java/test/testClass2.java
+++ src/main/java/test/testClass2.java
@@ -7,8 +7,8 @@ public class testClass2 {
 
     public void testReset2(){
         int i =0;
-        i=i+1;
-        System.out.println("this is a testreset 2"+i);
+        i = i + 1;
+        System.out.println("this is a testreset 2+hard commit" + i);
     }
 
 }
