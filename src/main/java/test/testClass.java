22f9c9db899aba712c84996668c5cd6c1fa0ac3c
fangshuangshuang <fangshuangshuang@meituan.com>
2021-08-17 14:37:14 +0800

    ceshiceshi

--- src/main/java/test/testClass.java
+++ src/main/java/test/testClass.java
@@ -3,8 +3,10 @@ package test;
 public class testClass {
     public void test1(){
         System.out.println("this is a test 1 branch test1");
+        System.out.println("this is a test 1 branch test1");
     }
     public void testReset(){
+        System.out.println("this is a testReset branch test1");
         System.out.println("this is a testReset branch test1");
     }
 }
--- src/main/java/test/testClass2.java
+++ src/main/java/test/testClass2.java
@@ -3,12 +3,14 @@ package test;
 public class testClass2 {
     public void test2(){
         System.out.println("this is a test 2 branch test1");
+        System.out.println("this is a test 2 branch test1");
     }
 
     public void testReset2(){
         int i =0;
         i=i+1;
-        System.out.println("this is a testreset 2 branch test1"+i);
+        System.out.println("this is a testreset 2 branch test1" + i);
+        System.out.println("this is a testreset 2 branch test1" + i);
     }
 
 }
