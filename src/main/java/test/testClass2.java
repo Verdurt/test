e6ab9dadd311de6e2cbf64411021ab30bb11b754
fangshuangshuang <fangshuangshuang@meituan.com>
2021-08-17 15:25:51 +0800

    mixed commit测试

--- src/main/java/test/testClass2.java
+++ src/main/java/test/testClass2.java
@@ -1,14 +1,19 @@
 package test;
 
 public class testClass2 {
-    public void test2(){
-        System.out.println("this is a test 2");
+    public void test2() {
+        System.out.println("this is a test 23333");
     }
 
-    public void testReset2(){
-        int i =0;
-        i=i+1;
-        System.out.println("this is a testreset 2"+i);
+    public void testReset2() {
+        int i = 0;
+        i = i + 1;
+        System.out.println("this is a testreset 23333" + i);
     }
 
+    public void testReset22() {
+        int i = 0;
+        i = i + 1;
+        System.out.println("this is a testreset 23333" + i);
+    }
 }
