032480e91fb194d4b7dd0c4a58b2d3e25877df28
fangshuangshuang <fangshuangshuang@meituan.com>
2021-08-17 14:55:04 +0800

    add soft test

--- src/main/java/test/testClass.java
+++ src/main/java/test/testClass.java
@@ -1,10 +1,15 @@
 package test;
 
 public class testClass {
-    public void test1(){
+    public void test1() {
         System.out.println("this is a test 1");
     }
-    public void testReset(){
+
+    public void testReset() {
+        System.out.println("this is a testReset");
+    }
+
+    public void testResettest1() {
         System.out.println("this is a testReset");
     }
 }
