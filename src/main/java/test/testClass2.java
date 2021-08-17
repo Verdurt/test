56a4206c2e1bc4df84a2d24a0f831c0f519d15f4
fangshuangshuang <fangshuangshuang@meituan.com>
2021-08-17 14:51:47 +0800

    add soft test

--- src/main/java/test/testClass2.java
+++ src/main/java/test/testClass2.java
@@ -1,14 +1,18 @@
 package test;
 
 public class testClass2 {
-    public void test2(){
-        System.out.println("this is a test 2");
+    public void test2() {
+        System.out.println("this is a test 2 branch test1");
+        System.out.println("this is a test 2 branch test1");
+        System.out.println("this is a test 2 branch soft");
     }
 
-    public void testReset2(){
-        int i =0;
-        i=i+1;
-        System.out.println("this is a testreset 2"+i);
+    public void testReset2() {
+        int i = 0;
+        i = i + 1;
+        System.out.println("this is a testreset 2 branch test1" + i);
+        System.out.println("this is a testreset 2 branch test1" + i);
+        System.out.println("this is a testreset 2 branch soft" + i);
     }
 
 }
