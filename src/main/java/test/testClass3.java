f5af5fc8fd841b7f43bf9dc5fc94cb1c29cc8cad
fangshuangshuang <fangshuangshuang@meituan.com>
2021-08-17 14:33:38 +0800

    test

--- src/main/java/test/testClass3.java
+++ src/main/java/test/testClass3.java
@@ -2,11 +2,11 @@ package test;
 
 public class testClass3 {
     public void test3(){
-        System.out.println("this is a test 3");
+        System.out.println("this is a test 3 branch test1");
     }
     public void testReset3(){
         int i =0;
-        i=i+2;
-        System.out.println("this is a testreset 3"+i);
+        i = i + 2;
+        System.out.println("this is a testreset 3 branch test1" + i);
     }
 }
