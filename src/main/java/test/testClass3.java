1cab2ae63ee64619f959bffe9cb69a2a13df40de
fangshuangshuang <fangshuangshuang@meituan.com>
2021-08-17 14:34:19 +0800

    branch test1

--- src/main/java/test/testClass3.java
+++ src/main/java/test/testClass3.java
@@ -7,6 +7,6 @@ public class testClass3 {
     public void testReset3(){
         int i =0;
         i = i + 2;
-        System.out.println("this is a testreset 3 branch test1" + i);
+        System.out.println("this is a testreset 3 branch test1 branch test1" + i);
     }
 }
