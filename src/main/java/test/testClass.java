0b13bebbafc3df032c08ce77a24fd30ba9d15d41
fangshuangshuang <fangshuangshuang@meituan.com>
2021-08-17 15:38:21 +0800

    maetr测试

--- src/main/java/test/testClass.java
+++ src/main/java/test/testClass.java
@@ -2,11 +2,12 @@ package test;
 
 public class testClass {
     public void test1() {
-        System.out.println("this is a test 1");
+        System.out.println("this is a test 12");
     }
 
     public void testReset() {
-        System.out.println("this is a testReset");
+        System.out.println("this is a testReset22");
+        System.out.println("this is a testReset23");
     }
 
     public void testResettest1() {
